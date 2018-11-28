a5ad5225f74b6bfea28457021b2b0ed093e2a180
robmelfi <melfiroberto@gmail.com>
2018-11-28 16:17:04 +0100

    fix tslint rules

--- src/main/webapp/app/modules/home/toggle-switch.tsx
+++ src/main/webapp/app/modules/home/toggle-switch.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button, ButtonGroup } from 'reactstrap';
 
 const toggleSwitch = props => {
-  return (
+  const content = (
     <ButtonGroup>
       <Button
         color="primary"
@@ -14,6 +14,7 @@ const toggleSwitch = props => {
         disabled={!props.status}>OFF</Button>
     </ButtonGroup>
   );
+  return content;
 };
 
 export default toggleSwitch;
