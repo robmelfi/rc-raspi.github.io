da6ea09b5038fcb1a325ca013af54a8fca0c96dc
robmelfi <melfiroberto@gmail.com>
2018-12-11 12:20:45 +0100

    front end - sensors management - UI i18n update

--- src/main/webapp/app/modules/home/TempHumWidget.tsx
+++ src/main/webapp/app/modules/home/TempHumWidget.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 
+import { Translate } from 'react-jhipster';
 import { Col } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const tempHumWidget = props =>
   <>
     <Col xs="5" className="shadow-sm p-2 m-2 text-center">
-      <div className="mb-3">Humidity</div>
+      <div className="mb-3">
+        <Translate contentKey="rcraspiApp.humidity.detail.title">
+          Humidity
+        </Translate>
+      </div>
       <div className="">{props.humidity ? props.humidity : '-'}<span>&nbsp;%&nbsp;</span><span><FontAwesomeIcon icon="tint" fixedWidth /></span></div>
     </Col>
     <Col xs="5" className="shadow-sm p-2 m-2 text-center">
-      <div className="mb-3">Temperature</div>
+      <div className="mb-3">
+        <Translate contentKey="rcraspiApp.temperature.detail.title">
+          Temperature
+        </Translate>
+      </div>
       <div className="">{props.temperature ? props.temperature : '-'}<span>&nbsp;C&#176;&nbsp;<FontAwesomeIcon icon="thermometer-half" fixedWidth /></span></div>
     </Col>
   </>;
