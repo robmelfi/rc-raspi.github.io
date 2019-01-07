a5352aba3972c73648441834da1587b35122c075
robmelfi <melfiroberto@gmail.com>
2019-01-07 18:59:52 +0100

    delete unused file

--- src/main/resources/data-h2.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO pin (id, name) VALUES (1, 'GPIO_00');
-INSERT INTO pin (id, name) VALUES (2, 'GPIO_01');
-INSERT INTO pin (id, name) VALUES (3, 'GPIO_02');
-INSERT INTO pin (id, name) VALUES (4, 'GPIO_03');
